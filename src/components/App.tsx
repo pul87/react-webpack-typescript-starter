8120a2a45624cd183de26ca0c271003afc585f58
Paolo Basso <paolo.basso87@gmail.com>
2017-02-17 12:23:01 +0100

    Added Redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,13 +5,14 @@ require("!style-loader!css-loader!sass-loader!./App.scss");
 const reactLogo = require('./react_logo.svg');
 
 export interface AppProps {
+    name?: string
 }
 
 export default class App extends React.Component<AppProps, undefined> {
     render() {
         return <div className="app">
             <h1>Hello World!</h1>
-            <p>Foo to the barz</p>
+            <p>Foo to the barzzzz</p>
             <img src={reactLogo}/>
         </div>;
     }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import * as React from "react";
 import {render} from "react-dom";
 import {AppContainer} from "react-hot-loader";
 import App from "./components/App";
-
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
 const rootEl = document.getElementById("root");
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 render(
     <AppContainer>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </AppContainer>,
     rootEl
 );
@@ -16,12 +22,20 @@ render(
 declare let module: {hot: any};
 
 if (module.hot) {
+
+    module.hot.accept("./reducers", () => {
+        const nextRootReducer = require("./reducers/index");
+        store.replaceReducer(nextRootReducer);
+    });
+
     module.hot.accept("./components/App", () => {
         const NewApp = require("./components/App").default;
 
         render(
             <AppContainer>
-                <NewApp/>
+                <Provider store={store}>
+                    <NewApp/>
+                </Provider>
             </AppContainer>,
             rootEl
         );
